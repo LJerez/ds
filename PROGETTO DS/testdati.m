34cbae0aa1ab178e35639edef479af35e874b0df
LJerez <solig9340@gmail.com>
2025-10-24 15:27:59 +0200

    Cambiamenti

--- PROGETTO DS/testdati.m
+++ PROGETTO DS/testdati.m
@@ -4,6 +4,8 @@ banche = readtable ("Export 23_10_2025 09_40.xlsx",Sheet=2);
 %VARIABLE CREATION
 nomicolonne = banche.Properties.VariableNames;
 quanteBanche=0;
+primadata = banche.LastDealStatusDate(1);
+ultimadata = banche.LastDealStatusDate(1);
 %FUNZIONE CHE CONTA IL NUMERO DI OPERAZIONI UNICHE
 % for i=1:height(banche)
 % 
@@ -13,13 +15,18 @@ quanteBanche=0;
 % 
 % end
 % quanteBanche
-dat = banche{:,"LastDealStatusDate"};
-varanni=0;
-for i=1:height(banche)
 
-    if banche.LastDealStatusDate(i) ~ NaT
-        varanni = varanni + 1;
+
+for i=1:height(banche)
+    datafusione = banche.LastDealStatusDate(i);
+    if isnat(datafusione) == 0
+        if datafusione < primadata
+            primadata = datafusione;
+        elseif datafusione > ultimadata
+            ultimadata = datafusione;
+        end
     end
 
 end
-varanni
+primadata
+ultimadata

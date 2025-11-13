03ba2e7dd89e65ac2c8d52ade62cdc5e9345f9ac
LJerez <solig9340@gmail.com>
2025-11-13 08:35:39 +0100

    Update testdati.m

--- PROGETTO DS/testdati.m
+++ PROGETTO DS/testdati.m
@@ -1,6 +1,7 @@
 %clc; clear; close all;
 % %IMPORTING DATASET 
 % % ~ NOT OPERATOR
-banche = readtable ("banchedata.xlsx",Sheet=2);
-newtab = tablefiltering(banche);
+%banche = readtable ("banchedata.xlsx",Sheet=2);
+%newtab = tablefiltering(banche);
 %writetable(newtab,'datasetBank.xlsx')
+banks=readtable("datasetBank.xlsx");

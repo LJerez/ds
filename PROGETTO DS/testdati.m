a25a14e62c65ab33c4a3d4de1a337ba710dee4ee
LJerez <solig9340@gmail.com>
2025-11-13 09:06:34 +0100

    spelta

--- PROGETTO DS/testdati.m
+++ PROGETTO DS/testdati.m
@@ -5,3 +5,30 @@
 %newtab = tablefiltering(banche);
 %writetable(newtab,'datasetBank.xlsx')
 banks=readtable("datasetBank.xlsx");
+
+% define variables
+FromBank = string(banks.FirmName);
+ToBanks = string(banks.SUB_Name);
+Quantity = banks.SubDir;
+
+G = digraph(FromBank,ToBanks,Quantity);
+Adj = adjacency(G);
+
+disp('Original Density')
+100*nnz(Adj)/(length(Adj)^2)
+
+
+
+[bin, binsize] = conncomp(G,"Type","weak");
+
+[~, idx] = max(binsize);         % index of the largest component
+nodes_in_GCC = find(bin == idx); % nodes belonging to it
+G_giant = subgraph(G, nodes_in_GCC);
+AdjG = adjacency(G_giant);
+
+disp('GCC Density')
+100*nnz(AdjG)/(length(AdjG)^2)
+
+Nodi = G_giant.Nodes;
+save('GCC_Bank.mat','AdjG',"Nodi")
+

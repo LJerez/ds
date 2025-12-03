4f4b0600d7c6d57eb9f6528c7ca477fc705b8b0d
LJerez <solig9340@gmail.com>
2025-12-03 17:17:00 +0100

    power law + pagerank

--- PROGETTO DS/tablefiltering.m
+++ PROGETTO DS/tablefiltering.m
@@ -1,7 +1,8 @@
 function [outtab] = tablefiltering(btab)
-btab = btab(:, {'Var1','CompanyNameLatinAlphabet','SUB_Name','SUB_Direct_'});
+btab = btab(:, {'Var1','CompanyNameLatinAlphabet','SUB_Name','SUB_Direct_','Turnover'});
 %CHANGING THE NAMES
 btab.Properties.VariableNames{'CompanyNameLatinAlphabet'} = 'FirmName';
+btab.Properties.VariableNames{'Turnover'} = 'TO';
 btab.Properties.VariableNames{'SUB_Direct_'} = 'SubDir';
 % notnumval={};
 % righevuote=0;
--- PROGETTO DS/testdati.m
+++ PROGETTO DS/testdati.m
@@ -1,9 +1,9 @@
 clc; clear; close all;
 % %IMPORTING DATASET 
 % % ~ NOT OPERATOR
-%banche = readtable ("banchedata.xlsx",Sheet=2);
-%newtab = tablefiltering(banche);
-%writetable(newtab,'datasetBank.xlsx')
+banche = readtable ("banchedata.xlsx",Sheet=2);
+newtab = tablefiltering(banche);
+writetable(newtab,'datasetBank.xlsx')
 banks=readtable("datasetBank.xlsx");
 
 % define variables
